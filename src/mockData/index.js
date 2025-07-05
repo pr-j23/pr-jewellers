34292ddfe77c01a79ec80798dab74dcca2c605fa
pr-j23 <pavanraj577@gmail.com>
2025-07-05 21:07:48 +0530

    rakhi category added

--- src/mockData/index.js
+++ src/mockData/index.js
@@ -301,13 +301,22 @@ export const categories = [
     slug: "bangles",
     description: "Traditional and modern bangles to elevate your style",
     image:
-      "https://images.unsplash.com/photo-1605100804763-247f67b3557e?auto=format&fit=crop&w=800",
+      "https://images.unsplash.com/photo-1611591437281-460bfbe1220a?auto=format&fit=crop&w=800",
   },
   {
     id: 8,
     name: "Bracelets",
     slug: "bracelets",
     description: "Charming bangles, tennis bracelets, and cuffs",
+    image:
+      "https://images.unsplash.com/photo-1605100804763-247f67b3557e?auto=format&fit=crop&w=800",
+  },
+  {
+    id: 9,
+    name: "Rakhi",
+    slug: "rakhi",
+    description:
+      "Elegant handcrafted bangles, festive bracelets, and beautiful cuffs to celebrate Raksha Bandhan.",
     image:
       "https://images.unsplash.com/photo-1611591437281-460bfbe1220a?auto=format&fit=crop&w=800",
   },
@@ -339,7 +348,7 @@ const dropDownCategories = [
   { value: "rings", label: "Rings" },
   { value: "necklaces", label: "Necklaces" },
   { value: "earrings", label: "Earrings" },
-  { value: "silver coins", label: "Silver Coins" },
+  { value: "silver coinsc", label: "Silver Coins" },
   { value: "anklets", label: "Anklets" },
   { value: "bangles", label: "Bangles" },
   { value: "bracelets", label: "Bracelets" },
